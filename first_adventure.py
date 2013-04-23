05f7473752701f6a84bbfa8252fd98e79e179b97
Michael Beauchemin <michael.beauchemin@gmail.com>
2013-04-23 14:15:49 -0700

    first attempt at main loop logic

--- first_adventure.py
+++ first_adventure.py
@@ -2,18 +2,38 @@
 # copyright
 
 import pygame
+# start system wide definitions
+# import colors constants
+# import system constants
+# import game constants
+# end system wide definitions
+
+# start internal classes
+# import classes
+# end internal classes
 
 def main():
-	# setup game
+	# start game setup
+	# initialize display
+	# initialize clock
+	# initialize sprites
+	# initialize game constants
+	# end game setup
 	
-	# start game loop
-	# poll and handle events
+	# start game loop (while done == False)
+		# poll and handle events (check for input)
+		# start event processing
+		# end event processing
 	
-	# update game elements
+		# update game elements (move objects)
+		# start game logic
+		# end game logic
 	
-	# draw surface
+		# draw surface (draw objects)
+		# start draw
+		# end draw
 	
-	# show surface
+		# show surface (update screen)
 	# end game loop
 	
 	# close game
