260f88230df9ec474a0c4ce1c229e37373790d20
Michael Beauchemin <michael.beauchemin@gmail.com>
2013-04-23 12:21:29 -0700

    first overarching game loop

---
+++ first_adventure.py
@@ -0,0 +1,20 @@
+# filename: first_adventure
+# copyright
+
+import pygame
+
+def main():
+	# setup game
+	
+	# start game loop
+	# poll and handle events
+	
+	# update game elements
+	
+	# draw surface
+	
+	# show surface
+	# end game loop
+	
+	# close game
+	
